f76e7502991fde693043776153e8116195c29fca
Majid Kargar <majidkargar01@gmail.com>
2024-04-17 14:01:52 +0330

    Feat: Add a feature to open the collect in a new floating window

--- lua/collect/init.lua
+++ lua/collect/init.lua
@@ -1,19 +1,53 @@
 local M = {}
 
+-- local collect_group = vim.api.nvim_create_augroup("CollectaGroup", { clear = true })
+
 function M.setup(opts)
 	-- Validate table
 	opts = opts or {}
 
-	local collect_name = opts.collect_name
-	local collectNameParameter = collect_name and collect_name or "collect.lua"
+	M.open(opts)
+end
 
-	M.open_collect(collectNameParameter)
+local create_win_config = function(opts)
+	-- local ui = vim.api.nvim_list_uis()[1]
+	-- local col = 12
+
+	-- if ui ~= nil then
+	-- 	col = math.max(ui.width - 13, 0)
+	-- end
+
+	local height = opts.height or 15
+	local width = opts.win_id or 60
+
+	return {
+		relative = 'editor',
+		row = math.floor(((vim.o.lines - height) / 2) - 1),
+		col = math.floor((vim.o.columns - width) / 2),
+		width = width,
+		height = height,
+		focusable = true,
+		border = "rounded",
+		title = opts.title or "Collect",
+		title_pos = opts.title_pos or "left"
+	}
+end
+
+local create_window = function(opts)
+	local buf_id = vim.api.nvim_create_buf(false, true)
+	local config = create_win_config(opts)
+	local win_id = vim.api.nvim_open_win(buf_id, true, config)
+
+	return buf_id, win_id
 end
 
 -- Opens a buffer that is called the collect buffer
-function M.open_collect(collect_name)
-	vim.keymap.set("n", "<Leader>cn", function()
-		vim.cmd(":vsplit " .. collect_name .. ".lua")
+---@class Open
+---@field title string 
+---@field title_pose string 
+function M.open(opts)
+	vim.keymap.set("n", "<leader>cn", function()
+		create_window(opts)
 	end)
 end
 
